0e7415298cf15668c153370b0a44a342f0610e39
Lorenzo Coslado <lorenzo.coslado@gmail.com>
2024-04-23 02:03:50 +0200

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 const today = moment();
 const newYear = moment().endOf('year');
 const diffDays = newYear.diff(today, 'days');
-const nextYear = newYear.year();
+const nextYear = newYear.year() + 1;
 
 let fileContent = fs.readFileSync('README.md', 'utf8');
 
@@ -14,6 +14,8 @@ if (index === -1) {
   console.error('Marker not found in README.md');
   process.exit(1);
 }
+
+fileContent = fileContent.replace(/\*\*\d+ days before \d+\*\* ⏱ days before new years\n/, '');
 
 const readmeContent = `\n\n**${diffDays} days before ${nextYear}** ⏱ days before new years\n`;
 fileContent = fileContent.slice(0, index + marker.length) + readmeContent + fileContent.slice(index + marker.length);
